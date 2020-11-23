61edea26d0c07d08ab103f065db33d1b971ed5fd
ajarek <61388692+ajarek@users.noreply.github.com>
2020-11-23 22:41:10 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+const bulboff = document.querySelector('.bulboff');
+const body = document.querySelector('body');
+const text = document.querySelector('h1');
+const tablica = [`url('img/bulb.png')`, `url('img/bulboff.png')`];
+let i = 0;
+bulboff.addEventListener('click', () => {
+   if (i === 0) {
+      bulboff.style.backgroundImage = tablica[i];
+      i = i + 1;
+      body.style.background = 'linear-gradient(90deg, rgba(255,255,255,1) 0%, rgba(252,219,0,1) 50%, rgba(255,255,255,1) 100%)';
+      text.style.textShadow = '-9px -4px 2px rgba(128,128,128,0.83)'
+   } else if (i === 1) {
+
+      bulboff.style.backgroundImage = tablica[i]
+      i = i - 1
+      body.style.background = '#ffffff'
+      text.style.textShadow = 'none'
+   }
+
+})
+
+const hamburger = document.querySelector('.hamburger');
+const navbar = document.querySelector('.navbar');
+hamburger.addEventListener('click', () => {
+   hamburger.classList.toggle('change')
+   navbar.classList.toggle('change1')
+})
